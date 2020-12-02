8804251c87259b0690844eab5fd729c2c4a772f1
Ethan Grebmeier <ethangrebmeier@gmail.com>
2020-12-02 10:45:43 -0800

    added enfore https

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
+var enforce = require('express-sslify');
+
+// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
+// a load balancer (e.g. Heroku). See further comments below
+app.use(enforce.HTTPS({trustProtoHeader: true}));
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
